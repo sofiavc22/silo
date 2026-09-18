c6d6ef70d4e912115b070c68cf35d4d21b095681
sofiavc22 <sofiavalverdecruz@gmail.com>
2026-09-18 16:30:53 -0600

    Configure SILO production domain

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -8,7 +8,7 @@ const pages = [
   ...['huixquilucan', 'naucalpan', 'metepec', 'toluca', 'tlalnepantla', 'miguel-hidalgo', 'benito-juarez', 'cuauhtemoc', 'alvaro-obregon', 'coyoacan'].map((slug) => ({ path: `zonas/${slug}`, priority: '0.8', changefreq: 'monthly' })),
 ];
 export const GET: APIRoute = ({ site }) => {
-  const origin = site?.toString().replace(/\/$/, '') || 'https://silo.mx';
+  const origin = site?.toString().replace(/\/$/, '') || 'https://siloseguridadmx.site';
   const lastmod = new Date().toISOString().slice(0, 10);
   return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${pages.map((page) => `<url><loc>${origin}/${page.path}</loc><lastmod>${lastmod}</lastmod><changefreq>${page.changefreq}</changefreq><priority>${page.priority}</priority></url>`).join('')}</urlset>`, { headers: { 'Content-Type': 'application/xml' } });
 };
