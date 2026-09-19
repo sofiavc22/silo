ee3c429a36680f5aaf3cd6061327b739748f3b2f
sofiavc22 <sofiavalverdecruz@gmail.com>
2026-09-18 19:45:40 -0600

    Use descriptive cameras service URL

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,7 +4,7 @@ const pages = [
   { path: '', priority: '1.0', changefreq: 'weekly' },
   { path: 'nosotros', priority: '0.6', changefreq: 'monthly' },
   { path: 'contacto', priority: '0.8', changefreq: 'monthly' },
-  ...['camaras-ring-nest', 'timbres-inteligentes', 'cerraduras-yale', 'automatizacion-residencial'].map((slug) => ({ path: `servicios/${slug}`, priority: '0.9', changefreq: 'monthly' })),
+  ...['instalacion-de-camaras-de-seguridad', 'timbres-inteligentes', 'cerraduras-yale', 'automatizacion-residencial'].map((slug) => ({ path: `servicios/${slug}`, priority: '0.9', changefreq: 'monthly' })),
   ...['huixquilucan', 'naucalpan', 'metepec', 'toluca', 'tlalnepantla', 'miguel-hidalgo', 'benito-juarez', 'cuauhtemoc', 'alvaro-obregon', 'coyoacan'].map((slug) => ({ path: `zonas/${slug}`, priority: '0.8', changefreq: 'monthly' })),
 ];
 export const GET: APIRoute = ({ site }) => {
